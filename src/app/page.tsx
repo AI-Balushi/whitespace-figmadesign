21eacee2172548a9bb8b32656205991fb4457370
AI-Balushi <waheedjdk@hotmail.com>
2024-12-23 23:44:06 +0500

    Class 12 Assignment

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Work from "./components/Work";
+import Footer from "./components/Footer";
+import Extension from "./components/Extend";
+import Customize from "./components/Customize";
+import YourWork from "./components/YourWork";
+import Sponsors from "./components/Sponsors";
+
+export default function Home() {
+  return (
+   <div>
+      <Navbar />
+      <Hero />
+      <Work />
+      <Extension />
+      <Customize />
+      <YourWork />
+      <Sponsors />
+      <Footer />
+   </div>
+  );
+}
